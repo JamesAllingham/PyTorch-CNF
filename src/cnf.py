58aa74346a513535134908d5e2b0c734302a4c28
JamesAllingham <james.allingham@gmail.com>
2020-04-02 20:05:39 +0100

    Added the CNF implementation.

---
+++ src/cnf.py
@@ -0,0 +1,84 @@
+"""Implementation of the CNF"""
+import torch
+import torch.nn as nn
+
+
+class CNF(nn.Module):
+    """Continuous Normalising Flow as described in "Neural Ordinary Differential Equations" by Chen et al. (2018)
+
+    Adapted from the NumPy implementation at: https://gist.github.com/rtqichen/91924063aa4cc95e7ef30b3a5491cc52
+    """
+    def __init__(self, in_out_dim, hidden_dim, width):
+        super().__init__()
+        self.in_out_dim = in_out_dim
+        self.hidden_dim = hidden_dim
+        self.width = width
+        self.hyper_net = HyperNetwork(in_out_dim, hidden_dim, width)
+
+    def forward(self, t, states):
+        z = states[0]
+        logp_z = states[1]
+
+        batchsize = z.shape[0]
+
+        with torch.set_grad_enabled(True):
+            z.requires_grad_(True)
+
+            W, B, U = self.hyper_net(t)
+
+            Z = torch.unsqueeze(z, 0).repeat(self.width, 1, 1)
+
+            h = torch.tanh(torch.matmul(Z, W) + B)
+            dz_dt = torch.matmul(h, U).mean(0)
+
+            dlogp_z_dt = -trace_df_dz(dz_dt, z).view(batchsize, 1)
+
+        return (dz_dt, dlogp_z_dt)
+
+
+def trace_df_dz(f, z):
+    """Calculates the trace of Jacobian df/dz.
+    Stolen from: https://github.com/rtqichen/ffjord/blob/master/lib/layers/odefunc.py#L13
+    """
+    sum_diag = 0.
+    for i in range(z.shape[1]):
+        sum_diag += torch.autograd.grad(f[:, i].sum(), z, create_graph=True)[0].contiguous()[:, i].contiguous()
+
+    return sum_diag.contiguous()
+
+
+class HyperNetwork(nn.Module):
+    """Hyper-network allowing f(z(t), t) to change with time.
+    """
+    def __init__(self, in_out_dim, hidden_dim, width):
+        super().__init__()
+
+        blocksize = width * in_out_dim
+        
+        self.fc1 = nn.Linear(1, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, 3 * blocksize + width)
+
+        self.in_out_dim = in_out_dim
+        self.hidden_dim = hidden_dim
+        self.width = width
+        self.blocksize = blocksize
+
+    def forward(self, t):
+        # pred params
+        params = t.reshape(1, 1)
+        params = torch.tanh(self.fc1(params))
+        params = torch.tanh(self.fc2(params))
+        params = self.fc3(params)
+
+        # restructure
+        params = params.reshape(-1)
+        W = params[:self.blocksize].reshape(self.width, self.in_out_dim, 1)
+
+        U = params[self.blocksize:2 * self.blocksize].reshape(self.width, 1, self.in_out_dim)
+
+        G = params[2 * self.blocksize:3 * self.blocksize].reshape(self.width, 1, self.in_out_dim)
+        U = U * torch.sigmoid(G)
+
+        B = params[3 * self.blocksize:].reshape(self.width, 1, 1)
+        return [W, B, U]
